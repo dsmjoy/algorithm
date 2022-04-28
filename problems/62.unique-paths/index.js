07e5a0b417677846b331073ad7e48ea3c8b7ed92
dsmjoy <justredmmm@outlook.com>
2022-04-28 15:17:51 +0800

    s

--- problems/62.unique-paths/index.js
+++ problems/62.unique-paths/index.js
@@ -5,28 +5,21 @@
  */
 var uniquePaths = function (m, n) {
 
-  const map = new Map();
   let count = 0;
 
-  const inMap = (x, y) => map.has(`${x},${y}`);
-
   const dfs = (x, y) => {
-
     if (x === n - 1 && y === m - 1) {
       count++;
       return;
     }
 
-    map.set(`${x},${y}`, 1);
-    if (x + 1 < n && !inMap(x + 1, y)) {
+    if (x + 1 < n) {
       dfs(x + 1, y);
     }
 
-    if (y + 1 < m && !inMap(x, y + 1)) {
+    if (y + 1 < m) {
       dfs(x, y + 1);
     }
-
-    map.delete(`${x},${y}`);
   }
 
   dfs(0, 0);
